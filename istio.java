39e47ecca020abdc595e90aef5b4c14b3fcf1991
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-01-03 13:31:48 +0000

    more improvements

--- istio.java
+++
@@ -1,5 +0,0 @@
-//DEPS io.fabric8:kubernetes-client:6.3.1
-//DEPS io.fabric8:generator-annotations:6.3.1
-//DEPS io.sundr:builder-annotations:0.93.1
-//DEPS org.projectlombok:lombok:1.18.24
-//SOURCES src/**.java

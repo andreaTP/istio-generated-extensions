325374c8f3c2545540608e52b3d4746b49c442e5
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-01-03 12:33:27 +0000

    first blood

---
+++ Readme.md
@@ -0,0 +1,6 @@
+
+Here it goes automation to directly fetch and publish generated Java Code from CRDs
+
+Schedule + Hash ? -> versions?
+Pull mode download the CRDs check an hash and republish? How to version?
+
---
+++ istio.java
@@ -0,0 +1,5 @@
+//DEPS io.fabric8:kubernetes-client:6.4-SNAPSHOT
+//DEPS io.fabric8:generator-annotations:6.4-SNAPSHOT
+//DEPS io.sundr:builder-annotations:0.93.1
+//DEPS org.projectlombok:lombok:1.18.24
+//SOURCES src/**.java
